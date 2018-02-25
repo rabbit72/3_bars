8025d685da1795811e296cc647c18e52b9d6927b
Daniil Shadrin <rabbit72rus@gmail.com>
2018-02-25 21:18:00 +0300

    update user interface

--- bars.py
+++ bars.py
@@ -2,13 +2,12 @@ import json
 import sys
 import re
 
-PATH_TO_FILE = 'bar_moscow_json'
 # PATH_TO_SEATS_COUNT = ['features']['properties']['Attributes']['SeatsCount']
 AVAILABLE_COMMANDS = ['1', '2', '3', 'exit']
 
 
-def load_data(filepath):
-    with open(filepath) as file_data:
+def load_data(file_path):
+    with open(file_path) as file_data:
         return json.loads(file_data.read())
 
 
@@ -24,7 +23,7 @@ def get_closest_bar(data, longitude, latitude):
     pass
 
 
-def get_user_input():
+def get_user_choose():
     print_for_user = (
         'Enter "1" - find the biggest bar in Moscow\n'
         'Enter "2" - find the smallest bar in Moscow\n'
@@ -50,15 +49,29 @@ def get_user_coordinates(*arg):
     return tuple(coordinate_list)
 
 
-if __name__ == '__main__':
-    bar_data = load_data(PATH_TO_FILE)
-    user_choose = get_user_input()
+def processing_choose(user_choose):
     if user_choose == 'exit':
-        sys.exit('Good bay! See you later!')
+        return None
     elif user_choose == '1':
-        print(get_biggest_bar(bar_data))
+        return get_biggest_bar(bar_data)
     elif user_choose == '2':
-        print(get_smallest_bar(bar_data))
+        return get_smallest_bar(bar_data)
     elif user_choose == '3':
         longitude, latitude = get_user_coordinates('longitude', 'latitude')
-        print(get_closest_bar(bar_data, longitude, latitude))
+        return get_closest_bar(bar_data, longitude, latitude)
+
+
+if __name__ == '__main__':
+    try:
+        path_bar_data = input('Enter path to file: ')
+        bar_data = load_data(path_bar_data)
+        necessary_bar = processing_choose(get_user_choose())
+        if necessary_bar is None:
+            sys.exit('Good bay! See you soon!')
+        else:
+            print(necessary_bar)
+    except FileNotFoundError:
+        sys.exit('File on the entered path was not found. '
+                 'Check path to file.')
+    except json.decoder.JSONDecodeError:
+        sys.exit('File on the entered path not JSON format')
